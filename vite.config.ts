cb3809e7506c6482b6831c59d7311a36fd70b583
David <wizzardx@gmail.com>
2024-09-18 06:26:19 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+})
