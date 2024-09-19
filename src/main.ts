2f32e869e148e4ad8f0600d8d94ea72ab5a516b4
David <wizzardx@gmail.com>
2024-09-19 20:07:29 +0200

    Remove counter and vite + typescript logos

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import "./style.css";
 import typescriptLogo from "./typescript.svg";
 import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 import { setupAddButton, setupResetButton } from "./calculator.ts";
 
 const appElement = document.querySelector<HTMLDivElement>("#app");
@@ -23,27 +22,7 @@ if (appElement) {
           Result: <span id="result-value"></span>
       </div>
     </div>
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="${viteLogo}" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://www.typescriptlang.org/" target="_blank">
-        <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-      </a>
-      <h1>Vite + TypeScript</h1>
-      <div class="card">
-        <button id="counter" type="button"></button>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and TypeScript logos to learn more
-      </p>
-    </div>
   `;
-}
-
-const counterElement = document.querySelector<HTMLButtonElement>("#counter");
-if (counterElement) {
-  setupCounter(counterElement);
 }
 
 const addButton = document.querySelector<HTMLButtonElement>("button#add");
